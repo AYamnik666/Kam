70b95591f56035111c266b1f695130671c1327a0
Stacy <Anastasiy.yamnik@gmail.com>
2019-07-25 10:37:46 +0600

    Add Route

--- kam/src/components/Navbar/Navbar.jsx
+++ kam/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
-/*
-let s ={
-	'nav': 'Navbar_nav__3ou9Q',
-	'item': 'Navbar_item__3qaF3'
-}
-*/
-
 const Navbar = () =>{  
 
         return (
             <nav className={s.nav}>
 			<div className={s.item}>
-				<a>Profile</a>
+				<a href="/profile">Profile</a>
 			</div>
 			<div className= {`${s.item} ${s.active}`}>
-				<a>Messages</a>
+				<a href="/dialogs">Messages</a>
 				</div>
 			<div className={s.item}>
-			<a>News</a>
+			<a href="/news">News</a>
 				</div>
 			<div className={s.item}> 
-			<a>Music</a>
+			<a href="/music">Music</a>
 				</div>
 				<div className={s.item}>
-			<a>Settinngs</a>
+			<a href="settings">Settings</a>
 				</div>
 		</nav>
         )
