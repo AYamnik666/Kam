86bba36876295ed6633c0cfaa266f2deed953f5a
Stacy <Anastasiy.yamnik@gmail.com>
2019-07-26 11:43:45 +0600

    add route message

--- kam/src/components/Dialogs/Dialogs.jsx
+++ kam/src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import  s from '../Dialogs/Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) =>{
     return (
         <div>
             <div className={s.dialogs}>
                 <div className={s.dialogItems}>
-                    <div className={s.dialog + ' ' + s.active }>John</div>
-                    <div className={s.dialog}>Bred</div>
-                    <div className={s.dialog}>Lola</div>
-                    <div className={s.dialog}>Mary</div>
-                    <div className={s.dialog}>Meth</div>
-                    <div className={s.dialog}>Poul</div>
+                    <div className={s.dialog + ' ' + s.active }>
+                        <NavLink to='dialogs/1' >John</NavLink></div>
+                    <div className={s.dialog}>
+                    <NavLink to='dialogs/2' >Bred</NavLink></div>
+                    <div className={s.dialog}>
+                    <NavLink to='dialogs/3' >Lola</NavLink></div>
+                    <div className={s.dialog}>
+                    <NavLink to='dialogs/4' >Mary</NavLink></div>
+                    <div className={s.dialog}>
+                    <NavLink to='dialogs/5' >Meth</NavLink></div>
+                    <div className={s.dialog}>
+                    <NavLink to='dialogs/5' >Poul</NavLink></div>
                 </div>
                 <div className={s.messages}>
                 <div className={s.message}>Hi</div>
