3db8acd03c555c78039b6f4c7cc71e9612a640f8
Stacy <Anastasiy.yamnik@gmail.com>
2019-07-26 12:37:50 +0600

    Add ProfileInfo

--- kam/src/components/Profile/Profile.jsx
+++ kam/src/components/Profile/Profile.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
 
         return (
             <div>
-			<div>
-			<img src='http://www.baltana.com/files/wallpapers-4/Rain-Background-Best-Wallpaper-14520.jpg'></img>
+			<ProfileInfo />		
+			<MyPosts hey='yo'/>
 			</div>
-			<div>
-				<img id='ava' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Red_Square._Visual_Realism_of_a_Peasant_Woman_in_Two_Dimensions.jpg/350px-Red_Square._Visual_Realism_of_a_Peasant_Woman_in_Two_Dimensions.jpg'></img>
-			</div>
-		
-			<div>
-			ava + description
-		</div>
-<MyPosts hey='yo'/>
-</div>
 				
         )
 
