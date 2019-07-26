674a57b1ef2bdbb420436d606367bc72fa10bccc
Stacy <Anastasiy.yamnik@gmail.com>
2019-07-26 09:42:36 +0600

    dialog css

--- kam/src/components/Dialogs/Dialogs.jsx
+++ kam/src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,21 @@ import  s from '../Dialogs/Dialogs.module.css';
 const Dialogs = (props) =>{
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogItems}>
+                    <div className={s.dialog + ' ' + s.active }>John</div>
+                    <div className={s.dialog}>Bred</div>
+                    <div className={s.dialog}>Lola</div>
+                    <div className={s.dialog}>Mary</div>
+                    <div className={s.dialog}>Meth</div>
+                    <div className={s.dialog}>Poul</div>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
