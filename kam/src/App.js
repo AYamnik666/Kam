5ac65e041ed7fd311cd0a2b54503a1e944d62e63
Stacy <Anastasiy.yamnik@gmail.com>
2019-07-30 16:32:22 +0600

    Edit

--- kam/src/App.js
+++ kam/src/App.js
@@ -9,6 +9,9 @@ import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import {BrowserRouter, Route} from 'react-router-dom';
+
+
+
 const App = () => {
     return (
 <BrowserRouter>
@@ -17,11 +20,18 @@ const App = () => {
 			<Navbar />
 	
 		<div className='app-wrapper-content'>
-<Route exact path='/dialogs' component={Dialogs} />
-<Route path='/profile' component={Profile} />
+
 <Route path='/news' component={News} />
 <Route path='/music' component={Music} />
 <Route path='/settings' component={Settings} />
+{/*
+<Route exact path='/dialogs' component={Dialogs} />
+<Route path='/profile' component={Profile} />*/}
+
+<Route path='/dialogs' render={() => <Dialogs />} />
+<Route path='/profile' render={() => <Profile />} />
+
+
 		
 		</div>
 	</div>
