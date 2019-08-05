81b6477a56e0d4af40f90130f3d4c3d73689597f
Stacy <Anastasiy.yamnik@gmail.com>
2019-08-05 15:39:21 +0600

    edit

--- kam/src/index.js
+++ kam/src/index.js
@@ -4,35 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let posts=[
-    {id: 1, message:'He? how are you?', likesCount:13},
-    {id: 2, message:'I\'s my first post', likesCount:11},
-    {id: 1, message:'Blabla', likesCount:1},
-    {id: 2, message:'Tralala', likesCount:11}
-]
+import state from './Redux/state';
 
-
-
-let dialogs=[
-    {    id:1,    name: 'Dima' }, 
-    {    id:2,    name: 'Bred' },
-    {    id:3,    name: 'Lola' },
-    {    id:4,    name: 'Mary' },
-    {    id:5,    name: 'Meth' },
-    {    id:6,    name: 'Liza' }
-]
-
-let messages=[
-    {id:1, message:'wwww'},
-    {id:2, message:'Hi'},
-    {id:3, message:'How are you?'},
-    {id:4, message:'Yo'}
-]
-
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs}  messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
