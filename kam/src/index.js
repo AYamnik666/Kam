373d24d15a6ee9d4658ce812d50e816fe3d663d5
Stacy <Anastasiy.yamnik@gmail.com>
2019-08-14 11:20:53 +0600

    edit 35

--- kam/src/index.js
+++ kam/src/index.js
@@ -1,13 +1,35 @@
 
 
 import App from './App';
+import state , {subscribe} from './Redux/state';
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from './render';
-import state from './Redux/state';
+
+import {addPost, updateNewPostText} from './Redux/state';
+//addPost('QQQQQQQQQQQQQWWWWWWWWWWWWWWEEEEEEEEEEEEEEEEEE');
+
+ let rerenderEntireTree = ()=>{
+
+ReactDOM.render(<App state={state} addPost={addPost } updateNewPostText={updateNewPostText}/>,
+     document.getElementById('root'));
+
+}
+
+
+
+
 
 
 
 rerenderEntireTree(state);
+
+subscribe (rerenderEntireTree);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
