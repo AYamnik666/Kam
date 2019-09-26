a9467b15b558b1a655f1a12bee90a4e029edcc0c
Stacy <Anastasiy.yamnik@gmail.com>
2019-09-26 17:31:51 +0600

    edit

--- kam/src/components/Profile/MyPosts/MyPosts.jsx
+++ kam/src/components/Profile/MyPosts/MyPosts.jsx
@@ -20,14 +20,15 @@ let addPost =() =>{
 
 	debugger;
 
-	props.addPost();
+	props.dispatch({type: 'ADD-POST'});
 
 }
 
 
 let onPostChange =() =>{
 	let text=newPostElement.current.value;
-	props.updateNewPostText(text);
+	props.dispatch({type: 'UPDATE-NEWPOST-TEXT', newText: text});
+
 }
 
 
