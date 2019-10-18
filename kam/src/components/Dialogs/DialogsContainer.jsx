74057128a3c8745455946f62960d069b88c38c10
Stacy <Anastasiy.yamnik@gmail.com>
2019-10-18 15:25:44 +0600

    edit

--- kam/src/components/Dialogs/DialogsContainer.jsx
+++ kam/src/components/Dialogs/DialogsContainer.jsx
@@ -1,21 +1,28 @@
+
+
+
+
 import React from 'react';
 import {updateNewMessageBodyCreator,sendMessageCreator} from '../../Redux/dialogs-reducer';
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState().dialogsPage;
+const DialogsContainer = () => {
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
+    return <StoreContext.Consumer>
+        { store => {
+            let onSendMessageClick = () => {
+                store.dispatch(sendMessageCreator());
+            }
+            let onNewMessageChange = (body) => {
+                store.dispatch(updateNewMessageBodyCreator(body));
+            }
+            return  <Dialogs updateNewMessageBody={onNewMessageChange}
+                     sendMessage={onSendMessageClick}
+                     dialogsPage={store.getState().dialogsPage}/>
+        }
     }
-
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
-
-    return <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick}
-                    dialogsPage={state}/>
+    </StoreContext.Consumer>
 }
 
 export default DialogsContainer;
-
