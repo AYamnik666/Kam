713de31ddf6369b595fd60e8c6fbc232ce46d9fc
Stacy <Anastasiy.yamnik@gmail.com>
2019-11-18 12:24:21 +0600

    edit

--- kam/src/components/Header/Header.jsx
+++ kam/src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import  s from '../Header/Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
         return (
             <header className={s.header}>
 			<img src='https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png'></img>
+
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink> }
+
+            </div>
 		</header>
         )
 
         }
-    
 
 
-export default Header; 
+
+export default Header;
