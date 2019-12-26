81d690fffacf63860dde8f19f0f19a72b8f83eed
Stacy <Anastasiy.yamnik@gmail.com>
2019-12-26 16:27:45 +0600

    edit

--- kam/src/components/Users/UsersContainer.js
+++ kam/src/components/Users/UsersContainer.js
@@ -7,8 +7,8 @@ import {
     setTotalUsersCount,
     toggleIsFetching,
     unfollow,
-    toggleFollowingProgress
-} from '../../Redux/users-reducer';
+    toggleFollowingProgress,
+    getUsersThunkCreator} from '../../Redux/users-reducer';
 import * as axios from "axios";
 import Users from "./Users";
 
@@ -18,13 +18,15 @@ import { usersAPI } from './../../api/api';
 
 class UsersContainer extends React.Component {
     componentDidMount() {
-        this.props.toggleIsFetching(true);
+this.props.getUsersThunkCreator();
+
+     /*   this.props.toggleIsFetching(true);
 
         usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                             this.props.toggleIsFetching(false);
                 this.props.setUsers(data.items);
                 this.props.setTotalUsersCount(data.totalCount);
-            });
+            });*/
     }
 
     onPageChanged = (pageNumber) => {
@@ -66,4 +68,8 @@ let mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps,
-    {follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress})(UsersContainer);
+    {follow, unfollow, setUsers,
+        setCurrentPage, setTotalUsersCount, toggleIsFetching,
+        toggleFollowingProgress, getUsersThunkCreator}) (UsersContainer);
+
+
