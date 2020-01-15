61e0e26d93258379e3cc1dd78c1085c2e4415906
Stacy <Anastasiy.yamnik@gmail.com>
2020-01-15 11:51:09 +0600

    edit

--- kam/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ kam/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../Redux/profile-reducer";
+import {addPostActionCreator} from "../../../Redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
 const mapStateToProps = (state) => {
     return {
         posts: state.profilePage.posts,
-        //newPostText: state.profilePage.newPostText
+        newPostText: state.profilePage.newPostText
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        updateNewPostText: (text) => {
-            let action = updateNewPostTextActionCreator(text);
-            dispatch(action);
-        },
-        addPost: () => {
-            dispatch(addPostActionCreator());
+        addPost: (newPostText) => {
+            dispatch(addPostActionCreator(newPostText));
         }
     }
 }
