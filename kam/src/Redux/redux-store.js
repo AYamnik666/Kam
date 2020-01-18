35c81a8c576ec3e759275e8678a98e051f5ceb50
Stacy <Anastasiy.yamnik@gmail.com>
2020-01-18 13:53:27 +0600

    edit

--- kam/src/Redux/redux-store.js
+++ kam/src/Redux/redux-store.js
@@ -1,25 +1,26 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import profileReduscer from './profile-reducer';
-import dialogsReduscer from './dialogs-reducer';
-import sidebarReduscer from './sidebar-reducer';
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
-import thunkMiddleware from 'redux-thunk';
+import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from 'redux-form'
+import appReducer from "./app-reducer";
 
 let reducers = combineReducers({
-    profilePage: profileReduscer,
-    dialogsPage: dialogsReduscer,
-    sidebar: sidebarReduscer,
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
-    form: formReducer
-
+    form: formReducer,
+    app: appReducer
 });
-
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
-window.store=store;
+window.store = store;
+
 
 export default store;
--- kam/src/components/Profile/ProfileContainer.jsx
+++ kam/src/components/Profile/ProfileContainer.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
-import {getUserProfile} from "../../Redux/profile-reducer";
-import {Redirect, withRouter} from "react-router-dom";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {getStatus, getUserProfile, updateStatus} from "../../Redux/profile-reducer";
+import {withRouter} from "react-router-dom";
 import {compose} from "redux";
-import { getStatus , updateStatus} from './../../Redux/profile-reducer';
-
-
 
 class ProfileContainer extends React.Component {
 
@@ -16,21 +11,23 @@ class ProfileContainer extends React.Component {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
+            if (!userId) {
+                this.props.history.push("/login");
+            }
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
 
-
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
+            <Profile {...this.props}
+                     profile={this.props.profile}
+                     status={this.props.status}
+                     updateStatus={this.props.updateStatus}/>
         )
     }
 }
-
-
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
@@ -41,6 +38,9 @@ let mapStateToProps = (state) => ({
 
 export default compose(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
-    withRouter,
-    withAuthRedirect
+    withRouter
 )(ProfileContainer);
+
+
+
+
