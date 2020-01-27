9025e9d31b410f6f4100840dbf571b5e46cd3b89
Stacy <Anastasiy.yamnik@gmail.com>
2020-01-27 16:54:31 +0600

    edit

--- kam/src/Redux/users-reducer.js
+++ kam/src/Redux/users-reducer.js
@@ -1,4 +1,5 @@
 import {usersAPI} from "../api/api";
+import {updateObjectInArray} from "../utils/object-helpers";
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
@@ -22,22 +23,24 @@ const usersReducer = (state = initialState, action) => {
         case FOLLOW:
             return {
                 ...state,
-                users: state.users.map( u =>  {
+                users: updateObjectInArray(state.users, action.userId, "id", {followed: true} )
+             /*   users: state.users.map( u =>  {
                     if (u.id === action.userId) {
                         return {...u, followed: true}
                     }
                     return u;
-                })
+                })*/
             }
         case UNFOLLOW:
             return {
                 ...state,
-                users: state.users.map( u =>  {
+                users: updateObjectInArray(state.users, action.userId, "id", {followed: false} )
+           /*     users: state.users.map( u =>  {
                     if (u.id === action.userId) {
                         return {...u, followed: false}
                     }
                     return u;
-                })
+                })*/
             }
         case SET_USERS: {
             return { ...state, users: action.users }
@@ -100,7 +103,7 @@ export const requestUsers = (page, pageSize) => {
 }
 
 
-const followUnfollow = async (dispatch, userId) => {
+const followUnfollowFlow = async (dispatch, userId, apiMethod, actionCreator) => {
     dispatch(toggleFollowingProgress(true, userId));
     let response=  await  usersAPI.follow(userId)
                 if (response.data.resultCode == 0) {
@@ -112,24 +115,15 @@ const followUnfollow = async (dispatch, userId) => {
 
 export const follow = (userId) => {
     return async (dispatch) => {
-        dispatch(toggleFollowingProgress(true, userId));
-    let response=  await  usersAPI.follow(userId)
-                if (response.data.resultCode == 0) {
-                    dispatch(followSuccess(userId));
-                }
-                dispatch(toggleFollowingProgress(false, userId));
+
+followUnfollowFlow(dispatch, userId, usersAPI.follow.bind(usersAPI), followSuccess);
           }
 }
 
 export const unfollow = (userId) => {
     return async (dispatch) => {
-        dispatch(toggleFollowingProgress(true, userId));
-     let response= await  usersAPI.unfollow(userId)
-                if (response.data.resultCode == 0) {
-                    dispatch(unfollowSuccess(userId));
-                }
-                dispatch(toggleFollowingProgress(false, userId));
-    }
+followUnfollowFlow(dispatch, userId, usersAPI.unfollow.bind(usersAPI), unfollowSuccess);
+          }
 }
 
 export default usersReducer;
