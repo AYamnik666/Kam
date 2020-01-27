d04314fc56ffadfacd44bb0005bf187f90167780
Stacy <Anastasiy.yamnik@gmail.com>
2020-01-27 10:11:26 +0600

    edit

--- kam/src/Redux/users-reducer.js
+++ kam/src/Redux/users-reducer.js
@@ -65,8 +65,6 @@ const usersReducer = (state = initialState, action) => {
     }
 }
 
-
-
 export const followSuccess = (userId) => ({type: FOLLOW, userId })
 export const unfollowSuccess = (userId) => ({type: UNFOLLOW, userId })
 export const setUsers = (users) => ({type: SET_USERS, users })
@@ -89,41 +87,48 @@ export const getUsers =(currentPage, pageSize) =>{
 
 
 export const requestUsers = (page, pageSize) => {
-    return (dispatch) => {
+    return async(dispatch) => {
         dispatch(toggleIsFetching(true));
         dispatch(setCurrentPage(page));
 
-        usersAPI.getUsers(page, pageSize).then(data => {
+        let data= await usersAPI.getUsers(page, pageSize);
+
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
             dispatch(setTotalUsersCount(data.totalCount));
-        });
     }
 }
 
-export const follow = (userId) => {
-    return (dispatch) => {
-        dispatch(toggleFollowingProgress(true, userId));
-        usersAPI.follow(userId)
-            .then(response => {
+
+const followUnfollow = async (dispatch, userId) => {
+    dispatch(toggleFollowingProgress(true, userId));
+    let response=  await  usersAPI.follow(userId)
                 if (response.data.resultCode == 0) {
                     dispatch(followSuccess(userId));
                 }
                 dispatch(toggleFollowingProgress(false, userId));
-            });
-    }
+
+}
+
+export const follow = (userId) => {
+    return async (dispatch) => {
+        dispatch(toggleFollowingProgress(true, userId));
+    let response=  await  usersAPI.follow(userId)
+                if (response.data.resultCode == 0) {
+                    dispatch(followSuccess(userId));
+                }
+                dispatch(toggleFollowingProgress(false, userId));
+          }
 }
 
 export const unfollow = (userId) => {
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch(toggleFollowingProgress(true, userId));
-        usersAPI.unfollow(userId)
-            .then(response => {
+     let response= await  usersAPI.unfollow(userId)
                 if (response.data.resultCode == 0) {
                     dispatch(unfollowSuccess(userId));
                 }
                 dispatch(toggleFollowingProgress(false, userId));
-            });
     }
 }
 
