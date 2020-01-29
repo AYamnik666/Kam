ecb0d681b0c5b202bd4965d9b65bad1fa55f5251
Stacy <Anastasiy.yamnik@gmail.com>
2020-01-29 12:21:53 +0600

    edit

--- kam/src/Redux/users-reducer.js
+++ kam/src/Redux/users-reducer.js
@@ -10,12 +10,13 @@ const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 let initialState = {
-    users: [ ],
-    pageSize: 5,
+    users: [],
+    pageSize: 10,
     totalUsersCount: 0,
-    currentPage: 2,
-    isFeathing: true,
-    followingInProgress: []
+    currentPage: 1,
+    isFetching: true,
+    followingInProgress: [],
+    fake: 10
 };
 
 
